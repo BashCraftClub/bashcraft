74a0495b8eb715b955a37dfcbeaae9a3ecd8dd14
Om Mishra <om.works01@gmail.com>
2024-09-27 23:39:29 +0530

    nav

--- app/(home)/components/navbar.tsx
+++ app/(home)/components/navbar.tsx
@@ -1,16 +1,16 @@
-import { ModeToggle } from '@/components/ui/theme-toggle'
-import { cn } from '@/lib/utils'
-import React from 'react'
+import { ModeToggle } from "@/components/ui/theme-toggle";
+import { cn } from "@/lib/utils";
+import React from "react";
 
-function Navbar({className} :{className?: string}) {
+function Navbar({ className }: { className?: string }) {
   return (
-    <nav className={cn('py-10 flex justify-between items-center', className)}>
-         <h1 className='text-2xl font-bold'>BashCraft</h1>
-         <div className='flex items-center gap-5'>
-            <ModeToggle />
-         </div>
+    <nav className={cn("py-10 flex justify-between items-center", className)}>
+      <h1 className="text-2xl font-bold">BashCraft</h1>
+      <div className="flex items-center justify-center gap-5">
+        <ModeToggle />
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,17 +1,14 @@
-import { ModeToggle } from '@/components/ui/theme-toggle'
-import React from 'react'
-import Navbar from './components/navbar'
+import { ModeToggle } from "@/components/ui/theme-toggle";
+import React from "react";
+import Navbar from "./components/navbar";
 
 export default function page() {
   return (
-    <div className='min-h-screen bg-black overflow-hidden'>
+    <div className="min-h-screen bg-black overflow-hidden">
       <div className=" max-w-7xl mx-auto p-5">
         <Navbar />
-        <div>
-          {"visitor@bashcraft:~$"}
-        </div>
-      </div>  
+        <div className="">{"visitor@bashcraft:~$"}</div>
+      </div>
     </div>
-    
-  )
+  );
 }
